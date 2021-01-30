bedc6192978386edb40419a60cc3c7845ce529d3
dhythm <yuta.okada.20@gmail.com>
2021-01-30 22:33:44 +0900

    feat(app): Customizing the Menu Icons(Tutorial)

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import PostIcon from '@material-ui/icons/Book';
+import UserIcon from '@material-ui/icons/Group';
 import jsonServerProvider from 'ra-data-json-server';
 import React from 'react';
 import { Admin, Resource } from 'react-admin';
@@ -12,8 +14,9 @@ const App = () => (
       list={PostList}
       edit={PostEdit}
       create={PostCreate}
+      icon={PostIcon}
     />
-    <Resource name="users" list={UserList} />
+    <Resource name="users" list={UserList} icon={UserIcon} />
   </Admin>
 );
 
--- src/posts.js
+++ src/posts.js
@@ -4,6 +4,7 @@ import {
   Datagrid,
   Edit,
   EditButton,
+  Filter,
   List,
   ReferenceField,
   ReferenceInput,
@@ -13,8 +14,21 @@ import {
   TextInput,
 } from 'react-admin';
 
+const PostTitle = ({ record }) => {
+  return <span>Post {record ? `"${record.title}"` : ''}</span>;
+};
+
+const PostFilter = (props) => (
+  <Filter {...props}>
+    <TextInput label="Search" source="q" alwaysOn />
+    <ReferenceInput label="User" source="userId" reference="users" allowEmpty>
+      <SelectInput optionText="name" />
+    </ReferenceInput>
+  </Filter>
+);
+
 export const PostList = (props) => (
-  <List {...props}>
+  <List filters={<PostFilter />} {...props}>
     <Datagrid>
       <TextField source="id" />
       <ReferenceField source="userId" reference="users">
@@ -27,7 +41,7 @@ export const PostList = (props) => (
 );
 
 export const PostEdit = (props) => (
-  <Edit {...props}>
+  <Edit title={<PostTitle />} {...props}>
     <SimpleForm>
       <ReferenceInput source="userId" reference="users">
         <SelectInput optionText="name" />
